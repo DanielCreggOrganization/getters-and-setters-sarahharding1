98fa29e5dc02abebd72e3cb100062027091885c9
sarahharding1 <G00374825@gmit.ie>
2020-02-27 14:45:14 +0000

    all complete

--- Main.java
+++ Main.java
@@ -1,3 +1,15 @@
 public class Main {
     // Main method
+
+    public static void main(String[] args) {
+        Person personObj = new Person();
+        personObj.setFirstName("Tom");
+        System.out.println(personObj.getFirstName());
+
+        Person personObj2 = new Person();
+        personObj2.setUserName("TheRedRhino");
+        System.out.println(personObj2.getUserName());
+    }
+
+
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,45 @@
-class Person {
+public class Person {
     // Instance Variables
-    
-    // Methods
+    private String firstName;
+    private String[] middleNames;
+    private String userName;
+
+        // Getter Methods
+    public String getFirstName(){
+
+        return firstName;
+    }
+    public String getMiddleNames(){
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < middleNames.length; i++){
+
+                sb.append(middleNames[i] + " ");
+        }
+        String middleNames = sb.toString();
+        return middleNames;
+    }
+    public String getUserName(){
+
+        return userName;
+    }
+
+    //Setter Method
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setMiddleNames(String middleNames){
+        this.middleNames = middleNames.split(" ");
+    }
+    public void setUserName(String userName){
+        if (userName.length() > 10){
+            this.userName = userName.substring(0, 10);
+        } else {
+            this.userName = userName;
+        }
+        
+    }
 }
+
+
